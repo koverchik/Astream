72d352e83f4776a69930b81810666b0fb76c3829
koverchik <koverchik.o@gmail.com>
2021-10-25 15:15:55 +0300

    fix: problem in main.yml file and change structure file tests

--- agoraStreaming/src/Screens/Live/__tests__/isBroadcaster.test.ts
+++ agoraStreaming/src/Screens/Live/__tests__/isBroadcaster.test.ts
@@ -1,5 +1,12 @@
+import {LiveType} from '../../../Navigation/types';
 import {isBroadcasterFunction} from '../helpers/isBroadcaster';
 
-test('is broadcaster?', () => {
-  expect(isBroadcasterFunction('create')).toBeTruthy();
+describe('Check broadcaster', () => {
+  test('true check result', () => {
+    expect(isBroadcasterFunction(LiveType.CREATE)).toBeTruthy();
+  });
+
+  test('false check result', () => {
+    expect(isBroadcasterFunction(LiveType.JOIN)).toBeFalsy();
+  });
 });
