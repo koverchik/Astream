ff59a655ab97ce37124307e66db594c147a8c18b
Kelek <kelekofficial@gmail.com>
2021-11-08 15:12:54 +0300

    add: exit button handler fix: user name and exit button positions

--- agoraStreaming/src/Components/ExitButton/ExitButton.tsx
+++ agoraStreaming/src/Components/ExitButton/ExitButton.tsx
@@ -2,10 +2,11 @@ import React, {FC} from 'react';
 import {Pressable} from 'react-native';
 import {ExitSvg} from '../../Icons/Exit/ExitSvg';
 import {styles} from './ExitButton.styles';
+import {ExitButtonPropsType} from './types';
 
-export const ExitButton: FC<any> = props => {
+export const ExitButton: FC<ExitButtonPropsType> = ({exitHandler}) => {
   return (
-    <Pressable style={styles.wrapper}>
+    <Pressable style={styles.wrapper} onPress={exitHandler}>
       <ExitSvg />
     </Pressable>
   );
