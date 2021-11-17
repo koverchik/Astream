7ea69a35e9ddfed37c6bc2252c95a5244a2560c3
koverchik <koverchik.o@gmail.com>
2021-11-17 18:44:48 +0300

    fix: deleted unnecessary file and create constants

--- agoraStreaming/src/Components/Header/styles.ts
+++ agoraStreaming/src/Components/Header/styles.ts
@@ -1,23 +1,25 @@
-import {StyleSheet, ViewStyle} from 'react-native';
+import {StyleSheet, TextStyle, ViewStyle} from 'react-native';
 
 type HeaderStyleType = {
   container: ViewStyle;
-  title: Text;
+  title: TextStyle;
   wrapperSectionIcons: ViewStyle;
   wrapperIcon: ViewStyle;
 };
+
+const SIZE_BLOCKS_ITEM = 35;
+
 export const styles = StyleSheet.create<HeaderStyleType>({
   container: {
     flexDirection: 'row',
-
     alignItems: 'center',
     justifyContent: 'space-between',
   },
   title: {
     color: 'white',
     backgroundColor: 'rgba(52, 52, 52, 0.8)',
-    borderRadius: 20,
-    height: 35,
+    borderRadius: SIZE_BLOCKS_ITEM / 2,
+    height: SIZE_BLOCKS_ITEM,
     textAlign: 'center',
     textAlignVertical: 'center',
     flex: 1,
@@ -28,9 +30,9 @@ export const styles = StyleSheet.create<HeaderStyleType>({
   },
   wrapperIcon: {
     backgroundColor: 'rgba(52, 52, 52, 0.8)',
-    borderRadius: 20,
-    height: 35,
-    width: 35,
+    borderRadius: SIZE_BLOCKS_ITEM / 2,
+    height: SIZE_BLOCKS_ITEM,
+    width: SIZE_BLOCKS_ITEM,
     margin: 5,
     justifyContent: 'center',
     alignItems: 'center',
--- agoraStreaming/src/Navigation/index.tsx
+++ agoraStreaming/src/Navigation/index.tsx
@@ -16,15 +16,15 @@ export const Navigation = () => {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={options}
-          // TODO:
-          // options={{
-          //   headerTransparent: true,
-          //   header: ({navigation, route, options, back}) => {
-          //     const title = getHeaderTitle(options, route.name);
-          //     return <HeaderApp title={title} />;
-          //   },
-          // }}
+          // options={options}
+          // TODO: You need use this fragment after create user page or notification
+          options={{
+            headerTransparent: true,
+            header: ({navigation, route, options, back}) => {
+              const title = getHeaderTitle(options, route.name);
+              return <HeaderApp title={title} />;
+            },
+          }}
         />
         <Stack.Screen name="Live" component={Live} options={options} />
       </Stack.Navigator>
