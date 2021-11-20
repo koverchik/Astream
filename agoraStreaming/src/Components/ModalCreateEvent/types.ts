da54343fab91e78251e1d11a9ce356c666fbac7c
koverchik <koverchik.o@gmail.com>
2021-11-20 11:51:29 +0300

    add: notification for events

--- agoraStreaming/src/Components/ModalCreateEvent/types.ts
+++ agoraStreaming/src/Components/ModalCreateEvent/types.ts
@@ -3,3 +3,9 @@ import {DateData} from 'react-native-calendars/src/types';
 export type ModalCreatEventType = {
   day: DateData['dateString'];
 };
+
+export type eventInDatabases = {
+  name: string;
+  video: boolean;
+  dateTime: string;
+};
--- agoraStreaming/src/Screens/Calendar/index.tsx
+++ agoraStreaming/src/Screens/Calendar/index.tsx
@@ -1,16 +1,64 @@
+import notifee, {TimestampTrigger, TriggerType} from '@notifee/react-native';
+import database from '@react-native-firebase/database';
 import React, {FC, useState} from 'react';
 import {View} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 import {ModalCreatEvent} from '../../Components/ModalCreateEvent';
+import {eventInDatabases} from '../../Components/ModalCreateEvent/types';
 
 export const ScreenCalendar: FC = () => {
   const dataSystem = new Date();
+
   const [chosenDay, setChoseDay] = useState(
     `${dataSystem.getFullYear()}-${
       dataSystem.getMonth() + 1
     }-${dataSystem.getDate()}`,
   );
+
+  database()
+    .ref(`/events/${chosenDay}`)
+    .once('value')
+    .then((snapshot) => {
+      const data: eventInDatabases[] = snapshot.val();
+      if (data != null) {
+        for (const key in data) {
+          const dateTimeNotification = Date.parse(data[key].dateTime) - 900000;
+          if (Date.now() < dateTimeNotification) {
+            onCreateTriggerNotification(data[key].dateTime, data[key].name);
+          }
+        }
+      }
+    });
+
+  async function onCreateTriggerNotification(
+    dataTime: eventInDatabases['dateTime'],
+    name: eventInDatabases['name'],
+  ) {
+    const dateNotification = new Date(dataTime);
+    const dateEvent = new Date(dataTime);
+    const dateTimeNotification = new Date(Date.parse(dataTime) - 900000);
+    dateNotification.setHours(
+      dateTimeNotification.getHours(),
+      dateTimeNotification.getMinutes(),
+    );
+
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: dateNotification.getTime(),
+    };
+
+    await notifee.createTriggerNotification(
+      {
+        title: name,
+        body: `The meeting will be at ${dateEvent.getHours()}:${dateEvent.getMinutes()}am`,
+        android: {
+          channelId: 'default',
+        },
+      },
+      trigger,
+    );
+  }
   return (
     <View>
       <Calendar
