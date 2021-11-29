3aa5983eb2dc8adb6e7b5404e3ca25cf41136865
Kelek <kelekofficial@gmail.com>
2021-11-29 14:34:59 +0300

    fix: fix redundant scroll for the last three days

--- agoraStreaming/src/Components/HorizontalCalendar/Helpers/scrollToSelectedDate.ts
+++ agoraStreaming/src/Components/HorizontalCalendar/Helpers/scrollToSelectedDate.ts
@@ -8,23 +8,32 @@ export const scrollToSelectedDate = (params: ScrollDateParamsType) => {
   const {month, year} = calendar;
 
   const dayInMonth = moment(`${year}-${month}`).daysInMonth();
+  const oneDayWidth = width / VISIBLE_COUNT_DAYS;
+  const threeDaysScroll = oneDayWidth * Math.floor(VISIBLE_COUNT_DAYS / 2);
 
   const monthMismatch = selectedMonth !== month;
   const yearMismatch = selectedYear !== year;
 
-  const oneDayWidth = width / VISIBLE_COUNT_DAYS;
-  const threeDaysScroll = oneDayWidth * Math.floor(VISIBLE_COUNT_DAYS / 2);
-  const scrollFromCurrentDay = selectedDayIndex
-    ? selectedDayIndex * oneDayWidth -
-      (selectedDayIndex < dayInMonth - 4 ? threeDaysScroll : 0)
-    : 0;
+  const scrollFromCurrentDayHandler = (index: typeof selectedDayIndex) => {
+    if (index) {
+      if (index > dayInMonth - 7) {
+        return (dayInMonth - 7) * oneDayWidth;
+      }
+
+      if (index < dayInMonth - 4) {
+        return index * oneDayWidth - threeDaysScroll;
+      }
+    }
+
+    return 0;
+  };
 
   if (monthMismatch || yearMismatch) {
     coords.current = {y: 0, x: 0};
   } else {
     coords.current = {
       y: 0,
-      x: scrollFromCurrentDay,
+      x: scrollFromCurrentDayHandler(selectedDayIndex),
     };
   }
 };
