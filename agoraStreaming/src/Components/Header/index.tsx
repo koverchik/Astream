76f4bfccdf227432d606e4223e26b6a7d4dc015d
Kelek <kelekofficial@gmail.com>
2021-11-30 13:01:22 +0300

    add: search input have been added without search request

--- agoraStreaming/src/Components/Header/index.tsx
+++ agoraStreaming/src/Components/Header/index.tsx
@@ -1,12 +1,21 @@
-import React, {FC} from 'react';
-import {Image, Text, View, useWindowDimensions} from 'react-native';
+import React, {FC, useRef, useState} from 'react';
+import {
+  Animated,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 
 import {useAppSelector} from '../../Redux/hooks';
 import {selectUser} from '../../Redux/selectors/AuthSelectors';
-import {HeaderStyles} from './styles';
+import {HeaderStyles, MARGIN, SIZE_BLOCKS_ITEM} from './styles';
 import {CustomHeaderPropsType} from './types';
 import {
   faBell,
+  faCheckCircle,
   faSearch,
   faUser,
   faUserPlus,
@@ -21,9 +30,45 @@ export const CustomHeader: FC<CustomHeaderPropsType> = (props) => {
 
   const user = useAppSelector(selectUser);
 
+  const inputAnimatedRef = useRef(new Animated.Value(0)).current;
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  const [searchMode, setSearchMode] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const onPressSearch = () => {
+    setSearchMode(true);
+    Animated.timing(inputAnimatedRef, {
+      toValue: width - SIZE_BLOCKS_ITEM * 2 + MARGIN,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const onPressCheck = () => {
+    Animated.timing(inputAnimatedRef, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+    setTimeout(() => {
+      setSearchMode(false);
+    }, 1720);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.wrapperSectionIcons}>
+      <Animated.View style={[styles.wrapperSectionIcons, {opacity}]}>
         <View style={styles.wrapperIcon}>
           {user?.photo ? (
             <Image source={{uri: user?.photo}} style={styles.image} />
@@ -34,18 +79,34 @@ export const CustomHeader: FC<CustomHeaderPropsType> = (props) => {
         <View style={styles.wrapperIcon}>
           <FontAwesomeIcon icon={faUserPlus} color={'white'} size={20} />
         </View>
-      </View>
-      <View style={styles.titleContainer}>
+      </Animated.View>
+      <Animated.View style={[styles.titleContainer, {opacity}]}>
         <Text style={styles.title}>{title}</Text>
-      </View>
+      </Animated.View>
       <View style={styles.wrapperSectionIcons}>
-        <View style={styles.wrapperIcon}>
+        <Animated.View style={[styles.wrapperIcon, {opacity}]}>
           <FontAwesomeIcon icon={faBell} color={'white'} size={18} />
-        </View>
-        <View style={styles.wrapperIcon}>
-          <FontAwesomeIcon icon={faSearch} color={'white'} size={18} />
-        </View>
+        </Animated.View>
+        {searchMode ? (
+          <TouchableOpacity style={styles.wrapperIcon} onPress={onPressCheck}>
+            <FontAwesomeIcon icon={faCheckCircle} color={'#7adaa8'} size={37} />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={styles.wrapperIcon} onPress={onPressSearch}>
+            <FontAwesomeIcon icon={faSearch} color={'white'} size={18} />
+          </TouchableOpacity>
+        )}
       </View>
+      {searchMode && (
+        <Animated.View
+          style={[styles.inputContainer, {width: inputAnimatedRef}]}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setSearchValue}
+            value={searchValue}
+          />
+        </Animated.View>
+      )}
     </View>
   );
 };
--- agoraStreaming/src/Components/Header/styles.ts
+++ agoraStreaming/src/Components/Header/styles.ts
@@ -13,9 +13,12 @@ type HeaderStyleType = {
   wrapperSectionIcons: ViewStyle;
   wrapperIcon: ViewStyle;
   image: ImageStyle;
+  input: ViewStyle;
+  inputContainer: ViewStyle;
 };
 
-const SIZE_BLOCKS_ITEM = 35;
+export const SIZE_BLOCKS_ITEM = 35;
+export const MARGIN = 5;
 
 export const HeaderStyles = (width: ScaledSize['width']) => {
   return StyleSheet.create<HeaderStyleType>({
@@ -38,14 +41,14 @@ export const HeaderStyles = (width: ScaledSize['width']) => {
     },
     wrapperSectionIcons: {
       flexDirection: 'row',
-      margin: 5,
+      margin: MARGIN,
     },
     wrapperIcon: {
       backgroundColor: 'rgba(52, 52, 52, 0.8)',
       borderRadius: SIZE_BLOCKS_ITEM / 2,
       height: SIZE_BLOCKS_ITEM,
       width: SIZE_BLOCKS_ITEM,
-      margin: 5,
+      margin: MARGIN,
       justifyContent: 'center',
       alignItems: 'center',
     },
@@ -53,6 +56,17 @@ export const HeaderStyles = (width: ScaledSize['width']) => {
       borderRadius: SIZE_BLOCKS_ITEM / 2,
       height: SIZE_BLOCKS_ITEM,
       width: SIZE_BLOCKS_ITEM,
+    },
+    inputContainer: {
+      position: 'absolute',
+      left: 10,
+    },
+    input: {
+      paddingLeft: 10,
+      height: SIZE_BLOCKS_ITEM,
+      borderRadius: SIZE_BLOCKS_ITEM / 2,
+      backgroundColor: 'rgba(52, 52, 52, 1)',
+      color: '#fff',
     },
   });
 };
