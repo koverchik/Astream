0702d7703a86b65d5e7459561fbf91e9412d9837
Kelek <kelekofficial@gmail.com>
2021-12-07 18:28:25 +0300

    fix: remove unused types

--- agoraStreaming/src/Redux/reducers/Home/types.ts
+++ agoraStreaming/src/Redux/reducers/Home/types.ts
@@ -1,14 +1,8 @@
 import {Region} from 'react-native-maps';
 
 import {ListChannelsType} from '../../../Screens/Home/types';
-import {HomeActionsType} from '../../actions/HomeActions/types';
 
 export type HomeInitialStateType = {
   coordinates: Region;
   listChannels: ListChannelsType[];
 };
-
-export type HomeReducerType = (
-  state: HomeInitialStateType,
-  action: HomeActionsType,
-) => HomeInitialStateType;
