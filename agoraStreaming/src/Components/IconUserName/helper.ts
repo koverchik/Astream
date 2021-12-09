5a545e6ab010d7e672f1b44a575088f7b5fb9414
koverchik <koverchik.o@gmail.com>
2021-12-09 11:25:38 +0300

    refactor: helper for animation position

--- agoraStreaming/src/Components/IconUserName/helper.ts
+++ agoraStreaming/src/Components/IconUserName/helper.ts
@@ -7,34 +7,33 @@ export const useValueForPosition = (
 ) => {
   const {height, width} = useWindowDimensions();
 
-  if (countUser === 2) {
-    return {
-      top: height / 4 - SIZE_USER_POINT / 2,
-      left: width / 2 - SIZE_USER_POINT / 2,
-    };
-  }
-
-  if (countUser === 3) {
-    if (index === 2) {
+  switch (countUser) {
+    case 2:
       return {
         top: height / 4 - SIZE_USER_POINT / 2,
         left: width / 2 - SIZE_USER_POINT / 2,
       };
-    } else {
+    case 3:
+      if (index === 2) {
+        return {
+          top: height / 4 - SIZE_USER_POINT / 2,
+          left: width / 2 - SIZE_USER_POINT / 2,
+        };
+      } else {
+        return {
+          top: height / 4 - SIZE_USER_POINT / 2,
+          left: width / 4 - SIZE_USER_POINT / 2,
+        };
+      }
+    case 4:
       return {
         top: height / 4 - SIZE_USER_POINT / 2,
-        left: width / 4 - SIZE_USER_POINT,
+        left: width / 4 - SIZE_USER_POINT / 2,
       };
-    }
+    default:
+      return {
+        top: height / 2 - SIZE_USER_POINT / 2,
+        left: width / 2 - SIZE_USER_POINT / 2,
+      };
   }
-  if (countUser === 4) {
-    return {
-      top: height / 4 - SIZE_USER_POINT / 2,
-      left: width / 4 - SIZE_USER_POINT,
-    };
-  }
-  return {
-    top: height / 2 - SIZE_USER_POINT / 2,
-    left: width / 2 - SIZE_USER_POINT / 2,
-  };
 };
