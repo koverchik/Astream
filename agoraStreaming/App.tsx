77fe97ac40da6a4a3eea1e51701c577d2cc066ba
Kelek <kelekofficial@gmail.com>
2021-12-10 12:46:18 +0300

    add: preloader

--- agoraStreaming/App.tsx
+++ agoraStreaming/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 
+import {Preloader} from './src/Components/Preloader';
 import {BottomTabs} from './src/Navigation/Tab';
 import {persistor, store} from './src/Redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -8,7 +9,9 @@ import {PersistGate} from 'redux-persist/integration/react';
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate
+        loading={<Preloader text={'Loading ...'} />}
+        persistor={persistor}>
         <BottomTabs />
       </PersistGate>
     </Provider>
