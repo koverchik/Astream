f103a50734e85e9ea37ab40b919418f1cf34be16
Kelek <kelekofficial@gmail.com>
2021-12-13 20:58:26 +0300

    add: online/offline indicator

--- agoraStreaming/src/Components/StreamEventItem/index.tsx
+++ agoraStreaming/src/Components/StreamEventItem/index.tsx
@@ -1,20 +1,35 @@
 import React, {FC} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import Animated, {interpolate, useAnimatedStyle} from 'react-native-reanimated';
+
+import {useNavigation} from '@react-navigation/native';
 
 import {COLORS} from '../../Colors/colors';
 import {CalendarSvg} from '../../Icons/CalendarSvg';
 import {DefaultAvatar} from '../../Icons/DefaultAvatar';
+import {LiveType, MainStackScreens} from '../../Navigation/Stack/types';
 import {addZeroForMinutes} from '../../Screens/Calendar/helpers/addZero';
+import {StackNavigationPropLive} from '../../Screens/Live/types';
 import {getStreamTypeIcon} from './helpers/getStreamTypeIcon';
-import {styles} from './styles';
+import {StreamItemStyles} from './styles';
 import {StreamEventItemPropsType} from './types';
 
 const START_ANIMATION = 95;
 
 export const StreamEventItem: FC<StreamEventItemPropsType> = (props) => {
-  const {time, type, name} = props.stream;
+  const {time, type, name, channelId, isVideo} = props.stream;
   const {translationY, index} = props;
+
+  const navigation = useNavigation<StackNavigationPropLive>();
+
+  const {width} = useWindowDimensions();
+  const styles = StreamItemStyles(width, channelId);
 
   const dataTime = new Date(time);
   const inputRange = [
@@ -22,6 +37,18 @@ export const StreamEventItem: FC<StreamEventItemPropsType> = (props) => {
     index * START_ANIMATION,
     (index + 1) * START_ANIMATION,
   ];
+
+  const onPressNotification = () => {
+    if (channelId) {
+      navigation.navigate(MainStackScreens.Live, {
+        type: LiveType.JOIN,
+        channelId,
+        isVideo,
+      });
+    } else {
+      Alert.alert('Stream is not started');
+    }
+  };
 
   const reanimatedStyle = useAnimatedStyle(() => {
     const scale = interpolate(translationY.value, inputRange, [1, 1, 0.7]);
@@ -49,8 +76,15 @@ export const StreamEventItem: FC<StreamEventItemPropsType> = (props) => {
         <Text style={styles.time}>
           {`${dataTime.getHours()}:${addZeroForMinutes(dataTime.getMinutes())}`}
         </Text>
+        <View style={styles.streamStatus}>
+          <View style={styles.streamStatusCircle} />
+          <Text>{channelId ? 'Online' : 'Offline'}</Text>
+        </View>
       </View>
-      <TouchableOpacity activeOpacity={0.5} style={styles.button}>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        style={styles.button}
+        onPress={onPressNotification}>
         <CalendarSvg color={COLORS.AZURE_RADIANCE} size={11} />
         <Text style={styles.buttonText}>Add to Call</Text>
       </TouchableOpacity>
--- agoraStreaming/src/Components/StreamEventItem/styles.ts
+++ agoraStreaming/src/Components/StreamEventItem/styles.ts
@@ -1,8 +1,7 @@
-import {Dimensions, StyleSheet, TextStyle, ViewStyle} from 'react-native';
+import {ScaledSize, StyleSheet, TextStyle, ViewStyle} from 'react-native';
 
 import {COLORS} from '../../Colors/colors';
-
-const {width} = Dimensions.get('window');
+import {StreamType} from '../../Screens/Calendar/types';
 
 const AVATAR_SIZE = 50;
 const STREAM_TYPE_SIZE = 20;
@@ -17,71 +16,98 @@ type StreamStylesType = {
   buttonText: TextStyle;
   time: TextStyle;
   name: TextStyle;
+  streamStatus: ViewStyle;
+  streamStatusCircle: ViewStyle;
 };
 
-const {BABY_BLUE_EYES, WHITE, SILVER_CHALICE, PORCELAIN, WILD_SAND} = COLORS;
+export const StreamItemStyles = (
+  width: ScaledSize['width'],
+  id: StreamType['channelId'],
+) => {
+  const {
+    CERISE_RED,
+    AZURE_RADIANCE,
+    BERMUDA,
+    BABY_BLUE_EYES,
+    WILD_SAND,
+    SILVER_CHALICE,
+    PORCELAIN,
+    WHITE,
+  } = COLORS;
 
-export const styles = StyleSheet.create<StreamStylesType>({
-  container: {
-    backgroundColor: WILD_SAND,
-    margin: 10,
-    borderColor: PORCELAIN,
-    borderWidth: 1,
-    flexDirection: 'row',
-    width: width * 0.8,
-    alignItems: 'center',
-    height: 75,
-    borderRadius: 10,
-    padding: 10,
-  },
-  avatarBox: {
-    width: AVATAR_SIZE,
-    height: AVATAR_SIZE + 5,
-    backgroundColor: WHITE,
-    borderRadius: AVATAR_SIZE / 2,
-  },
-  avatar: {
-    width: AVATAR_SIZE,
-    height: AVATAR_SIZE,
-    borderRadius: AVATAR_SIZE / 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: WHITE,
-  },
-  type: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    height: STREAM_TYPE_SIZE,
-    width: STREAM_TYPE_SIZE,
-    borderRadius: STREAM_TYPE_SIZE / 2,
-    backgroundColor: BABY_BLUE_EYES,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  middleBox: {
-    flex: 1,
-    height: '100%',
-    marginHorizontal: 10,
-  },
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '70%',
-    paddingHorizontal: 10,
-    borderRadius: 20,
-    backgroundColor: BABY_BLUE_EYES,
-  },
-  buttonText: {
-    color: COLORS.AZURE_RADIANCE,
-    fontSize: 10,
-    fontWeight: 'bold',
-  },
-  time: {
-    color: SILVER_CHALICE,
-  },
-  name: {
-    fontWeight: 'bold',
-  },
-});
+  return StyleSheet.create<StreamStylesType>({
+    container: {
+      backgroundColor: WILD_SAND,
+      margin: 10,
+      borderColor: PORCELAIN,
+      borderWidth: 1,
+      flexDirection: 'row',
+      width: width * 0.8,
+      alignItems: 'center',
+      height: 75,
+      borderRadius: 10,
+      padding: 10,
+    },
+    avatarBox: {
+      width: AVATAR_SIZE,
+      height: AVATAR_SIZE + 5,
+      backgroundColor: WHITE,
+      borderRadius: AVATAR_SIZE / 2,
+    },
+    avatar: {
+      width: AVATAR_SIZE,
+      height: AVATAR_SIZE,
+      borderRadius: AVATAR_SIZE / 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: WHITE,
+    },
+    type: {
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      height: STREAM_TYPE_SIZE,
+      width: STREAM_TYPE_SIZE,
+      borderRadius: STREAM_TYPE_SIZE / 2,
+      backgroundColor: BABY_BLUE_EYES,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    middleBox: {
+      flex: 1,
+      height: '100%',
+      marginHorizontal: 10,
+    },
+    button: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '70%',
+      paddingHorizontal: 10,
+      borderRadius: 20,
+      backgroundColor: BABY_BLUE_EYES,
+    },
+    buttonText: {
+      color: AZURE_RADIANCE,
+      fontSize: 10,
+      fontWeight: 'bold',
+    },
+    time: {
+      color: SILVER_CHALICE,
+    },
+    name: {
+      fontWeight: 'bold',
+    },
+    streamStatus: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    streamStatusCircle: {
+      height: 10,
+      width: 10,
+      backgroundColor: id ? BERMUDA : CERISE_RED,
+      borderRadius: 5,
+      marginRight: 2,
+    },
+  });
+};
