3d2c3d3b310fe5cf0b3a882655eb4eb82935e3d9
Kelek <kelekofficial@gmail.com>
2021-12-14 11:29:43 +0300

    fix: function refactoring with async await

--- agoraStreaming/src/Screens/Live/helpers/findKeyDataInDatabase.ts
+++ agoraStreaming/src/Screens/Live/helpers/findKeyDataInDatabase.ts
@@ -1,16 +1,13 @@
 import database from '@react-native-firebase/database';
 
-export const findKeyDataInDatabase = async (channelId: string) => {
-  const result = await database()
-    .ref('/channels/')
-    .once('value')
-    .then((snapshot) => {
-      const allDataChannels = snapshot.val();
-      for (const channel in allDataChannels) {
-        if (allDataChannels[channel].channelId === channelId) {
-          return channel;
-        }
-      }
-    });
-  return result;
+export const findKeyDataInDatabase = async (
+  channelId: string,
+): Promise<string | void> => {
+  const snapshot = await database().ref('/channels/').once('value');
+  const allDataChannels = snapshot.val();
+  for (const channel in allDataChannels) {
+    if (allDataChannels[channel].channelId === channelId) {
+      return channel;
+    }
+  }
 };
