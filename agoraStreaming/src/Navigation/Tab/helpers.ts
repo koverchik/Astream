f605fcb0344b2be7d35392758503c06bebbde3cb
koverchik <koverchik.o@gmail.com>
2021-12-16 11:16:03 +0300

    fix: type for function takeNameScreen

--- agoraStreaming/src/Navigation/Tab/helpers.ts
+++ agoraStreaming/src/Navigation/Tab/helpers.ts
@@ -1,27 +1,20 @@
 import {
   NavigationContainerProps,
   NavigationState,
-  PartialState,
 } from '@react-navigation/native';
 
 import analytics from '@react-native-firebase/analytics';
 
 import {AnalyticsType} from '../../Types/universalTypes';
 
-const takeNameScreen = (
-  state: NavigationState | PartialState<NavigationState> | undefined,
-): string | undefined => {
-  if (state) {
-    const {index, routeNames, routes} = state;
+const takeNameScreen = (state: NavigationState): string => {
+  const {index, routeNames, routes} = state;
 
-    if (index && routeNames && routes) {
-      if (!Object.prototype.hasOwnProperty.call(routes[index], 'state')) {
-        return routeNames[index];
-      }
-
-      return takeNameScreen(routes[index]['state']);
-    }
+  if (!Object.prototype.hasOwnProperty.call(routes[index], 'state')) {
+    return routeNames?.[index];
   }
+
+  return takeNameScreen(routes[index]['state']);
 };
 
 export const eventChangeScreen: NavigationContainerProps['onStateChange'] = (
