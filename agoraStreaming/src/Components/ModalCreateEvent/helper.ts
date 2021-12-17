f4b96bce67d2408d04d93b43273d377bd81b280f
koverchik <koverchik.o@gmail.com>
2021-12-17 12:27:07 +0300

    fix: add empty line

--- agoraStreaming/src/Components/ModalCreateEvent/helper.ts
+++ agoraStreaming/src/Components/ModalCreateEvent/helper.ts
@@ -2,5 +2,6 @@ import remoteConfig from '@react-native-firebase/remote-config';
 
 export const remoteConfigTypeStream = async (key: string) => {
   await remoteConfig().fetchAndActivate();
+
   return remoteConfig().getBoolean(key);
 };
