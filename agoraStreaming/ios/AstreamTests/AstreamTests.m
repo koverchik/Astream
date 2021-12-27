846ad4441be4a12209ab8560fe2f25231dcb00ac
koverchik <koverchik.o@gmail.com>
2021-12-27 17:41:37 +0300

    add: rename project

--- agoraStreaming/ios/AstreamTests/AstreamTests.m
+++ agoraStreaming/ios/AstreamTests/AstreamTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface agoraStreamingTests : XCTestCase
+@interface AstreamTests : XCTestCase
 
 @end
 
-@implementation agoraStreamingTests
+@implementation AstreamTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
